aa78f12d2ad1eb80a29802815e4e1ae4e7599dff
bumloner <bumloner@yandex.ru>
2020-09-27 02:36:31 +0700

    Edited README.md

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=notes_yii2',
+    'dsn' => 'mysql:host=localhost;dbname=noteboard',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
